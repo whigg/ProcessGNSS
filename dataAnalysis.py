f58091ce6dc26ddc9c773f70fd13a82b74317f46
derekpickell <37055625+derekpickell@users.noreply.github.com>
2021-10-06 09:57:18 -0400

    updated plot functions

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -7,6 +7,8 @@ import statistics
 import matplotlib.pyplot as plt
 import sys, os, glob
 
+NUM_OBSERVATIONS = 5700 #at 15s sample rate, approx 1 day
+
 def parse_file(file_name):
     x = []
     y = []
@@ -20,23 +22,29 @@ def parse_file(file_name):
             x.append(float(values[2]))
             y.append(float(values[3]))
             z.append(float(values[4]))
+            t.append(float(values[1]))
+
+    return x, y, z, t
+        
+def det_sample(t, x0, y0, z0):
+    """
+    determines sample rate of data, if not 15s, then decimate
+    also curtails array to fixed time
+    """
+    if ((t[2]-t[1])==1):
+        x = x0[0::15]
+        y = y0[0::15]
+        z = z0[0::15]
+        print("Sample Rate 1Hz: Decimating")
+    else:
+        print("Sample Rate 15s")
+
+    #curtail array length NUM_OBSERVATIONS
+    x = x0[:NUM_OBSERVATIONS]
+    y = y0[:NUM_OBSERVATIONS]
+    z = z0[:NUM_OBSERVATIONS]
 
     return x, y, z
-        
-def plot_xyz(x, y, z):
-    xplot = plt.figure(1)
-    plt.plot(x)
-    plt.title("x position (m)")
-
-    yplot = plt.figure(2)
-    plt.plot(y)
-    plt.title("y position (m)")
-
-    zplot = plt.figure(3)
-    plt.plot(z)
-    plt.title("z position (m)")
-    
-    plt.show()
 
 def calc_statistics(x, y, z):
     """
@@ -45,20 +53,44 @@ def calc_statistics(x, y, z):
     """
     meanx = statistics.mean(x) 
     stdx = statistics.stdev(x)
+    meany = statistics.mean(y) 
+    stdy = statistics.stdev(y)
+    meanz = statistics.mean(z) 
+    stdz = statistics.stdev(z)
     print ("x mean (m): ", meanx)
     print ("x stdev (m): ", stdx)
     print ("x sample size: ", len(x))
+    print ("z mean (m): ", meanz)
+    print ("z stdev (m): ", stdz)
+    print ("z sample size: ", len(z))
 
+    #ploting
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    fig.suptitle("ECEF positions")
+
+    ax1.plot(x)
+    ax1.set_title(f"x position (m), mean: {meanx}, stdev: {stdx}", fontsize = 8)
+    #ax1.text(3, 8, "test", fontsize=15, color='red')
+
+    ax2.plot(y)
+    ax2.set_title(f"y position (m), mean: {meany}, stdev: {stdy}", fontsize = 8)
+
+    ax3.plot(z)
+    ax3.set_title(f"z position (m), mean: {meanz}, stdev: {stdz}", fontsize = 8)
+    
+    plt.show()
 
 
 if __name__ == '__main__':
-     script_dir = os.path.dirname(__file__)
-     path = os.path.join(script_dir, "tempData", "*.pos") 
-     file_name = glob.glob(path)
-     x, y, z = parse_file(file_name[0])
-     
-     calc_statistics(x, y, z)
-     #plot_xyz(x, y, z)
+    print("Pullling .pos file from /tempData folder...")
+    script_dir = os.path.dirname(__file__)
+    path = os.path.join(script_dir, "tempData", "*.pos") 
+    file_name = glob.glob(path)
+
+    #Calculations:
+    x, y, z, t = parse_file(file_name[0])
+    x1, y1, z1 = det_sample(t, x, y, z) 
+    calc_statistics(x1, y1, z1)
      
 
 
---
+++ processTrimble/getTrimble.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+yes="y"
+no="n"
+dat="dat"
+o=".obs"
+n=".gps"
+
+echo Enter filename, without .dat extension
+read filename
+
+echo Do you want to process RAW to RINEX? y/n
+read response
+
+BASEDIR=$(dirname "$0")
+
+if [ $response == $yes ]; then
+
+    datFILE="$filename.$dat"
+    echo RINEX now processing: $datFILE notice: currently configured only for GPS nav messages
+    cd $BASEDIR
+
+    ./teqc +nav $filename".gps" $filename".dat" > $filename".obs"
+fi
+
+
+echo Do you want to process position? y/n
+read response
+
+if [ $response == $yes ]; then
+    OBFILE="$filename$o"
+    NAVFILE="$filename$n"
+    
+    OB=$BASEDIR"/"$OBFILE
+    NAV=$BASEDIR"/"$NAVFILE
+    echo $OBFILE
+    echo $NAVFILE
+    cd $BASEDIR
+    cd ..
+    cd RTKLIB-demo5/app/consapp/rnx2rtkp/gcc
+    ./rnx2rtkp -p 0 -m 15 -e -u -o $filename".pos" $OB $NAV
+    mv $filename".pos" $BASEDIR
+    mv $filename"_events.pos" $BASEDIR
+    
+ fi
+
+echo Do you want to process statistics? y/n
+read response
+
+if [ $response == $yes ]; then
+    echo Python will process the .pos file moved to /tempData
+    cd $BASEDIR
+    cd ..
+    source env/bin/activate
+    python3 dataAnalysis.py
+ fi

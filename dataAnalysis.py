91a5b56078345b6e77de323ec56385602752c150
derekpickell <37055625+derekpickell@users.noreply.github.com>
2021-11-15 15:09:13 -0500

    Update dataAnalysis.py

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,7 +1,8 @@
 # DEREK PICKELL
 # DATA ANALYSIS RTKLIB .POS FILES, ECEF
+# INPUTS: /tempData requires at least 1 .pos file
+# .POS Must be in ECEF, with time in YYYY/MM/DD HH:MM:SS.SSSS
 
-#import scipy
 import numpy as np
 import statistics
 import sys, os, glob
@@ -20,9 +21,10 @@ plt.rc('ytick', labelsize=6)
 plot_lw = 0.5
 title_size = 10
 label_size = 8
+lim_range = 10
 ########
 
-
+########HELPER FUNCTIONS
 def parse_file(file_name):
     """
     INPUT: .obs file with ECEF positions (x, y, z, t)
@@ -49,8 +51,7 @@ def parse_file(file_name):
         
 def det_sample(x0, y0, z0, t0, case=1): #time=86400
     """
-    determines sample rate of data, if not 15s, then decimate
-    also curtails array to fixed time NUM_OBSERVATIONS
+    If sample rate is 1Hz, decide whether to decimate to 15s or maintain
     """
     if case==15: 
             x1 = x0[0::15]
@@ -71,101 +72,6 @@ def det_sample(x0, y0, z0, t0, case=1): #time=86400
   
     return x1, y1, z1, t1
 
-def obs_time(obs_time, x, y, z, t):
-    """
-    shortens data to specific length of time
-    """
-    x2 = x1[:obs_time]
-    y2 = y1[:obs_time]
-    z2 = z1[:obs_time]
-    t2 = t1[:obs_time]
-
-def calc_ECEFstatistics(x, y, z, t):
-    """
-    What about # of satellites? 
-    Ensure sample #/sample rate is same for Trimble Data...
-    """
-
-    meanx = statistics.mean(x) 
-    meanx_line = [meanx] * len(x)
-    stdx = statistics.pstdev(x)
-    stdx3 = 3*stdx
-    stdx_line = [meanx+stdx*3] *len(x)
-    stdx_line2 = [meanx+stdx*-3] *len(x)
-
-    meany = statistics.mean(y) 
-    stdy = statistics.pstdev(y)
-    meany_line = [meany] * len(y)
-    stdy3 = 3*stdy
-    stdy_line = [meany+stdy*3] *len(y)
-    stdy_line2 = [meany+stdy*-3] *len(y)
-
-    meanz = statistics.mean(z) 
-    stdz = statistics.pstdev(z)
-    meanz_line = [meanz] * len(z)
-    stdz3 = 3*stdz
-    stdz_line = [meanz+stdz*3] *len(z)
-    stdz_line2 = [meanz+stdz*-3] *len(z)
-
-
-    #ploting
-    fig, ((ax1, ax4), (ax2, ax5), (ax3, ax6)) = plt.subplots(3, 2, figsize=(12,8), gridspec_kw={'width_ratios': [3, 1]}) #, sharex=True
-    fig.suptitle(f"ECEF Positions from " +str(t[0]) + " to "+ str(t[-1]))
-    lim_range = 10
-
-    ax1.plot_date(t, x, marker='None', ls='-', lw=plot_lw)
-    ax1.plot_date(t, meanx_line, marker='None',color='red', lw=plot_lw, ls='--', label="x average: "+'%.11g' % meanx +"m")
-    ax1.plot_date(t, stdx_line, marker='None',color='green', lw=plot_lw, ls='--', label="3\u03C3: +/-"+ '%.5g' % stdx3 +"m")
-    ax1.plot_date(t, stdx_line2, marker='None', color='green', lw=plot_lw, ls='--')
-    ax1.set_title("x position (m)", fontsize = title_size) #, stdev: "'%.5g' % stdx
-    ax1.legend(loc='lower left', fontsize = label_size)
-    ax1.set_ylim(float('%.11g' % meanx) - lim_range, float('%.11g' % meanx) + lim_range)
-    ax1.margins(x=0)
-    ax1.set_xticklabels([])
-    
-    ax2.plot_date(t, y, marker='None', ls='-', lw=plot_lw)
-    ax2.plot_date(t, meany_line, marker='None',color='red', lw=plot_lw, ls='--', label="y average: "+'%.11g' % meany +"m")
-    ax2.plot_date(t, stdy_line, marker='None',color='green', lw=plot_lw, ls='--', label="3\u03C3: +/-"+ '%.5g' % stdy3 +"m")
-    ax2.plot_date(t, stdy_line2, marker='None',color='green', lw=plot_lw, ls='--')
-    ax2.set_title("y position (m)", fontsize = title_size)
-    ax2.legend(loc='lower left', fontsize = label_size)
-    ax2.set_ylim(float('%.11g' % meany) - lim_range, float('%.11g' % meany) + lim_range)
-    ax2.margins(x=0)
-    ax2.set_xticklabels([])
-
-    ax3.plot_date(t, z, marker='None', ls='-', lw=plot_lw)
-    ax3.plot_date(t, meanz_line, marker='None',color='red', lw=plot_lw, ls='--', label="z average: "+'%.11g' % meanz +"m")
-    ax3.plot_date(t, stdz_line, marker='None',color='green', lw=plot_lw, ls='--', label="3\u03C3: +/-"+ '%.5g' % stdz3 +"m")
-    ax3.plot_date(t, stdz_line2, marker='None', color='green', lw=plot_lw, ls='--')
-    ax3.set_title("z position (m)", fontsize = title_size)
-    ax3.legend(loc='lower left', fontsize = label_size)
-
-    ax3.set_xticklabels(ax3.get_xticks(), rotation = 45)
-    ax3.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M:%S"))
-    ax3.set(xlabel="Time")
-    ax3.set_ylim(float('%.11g' % meanz) - lim_range, float('%.11g' % meanz) + lim_range)
-    ax3.margins(x=0)
-
-    #histograms
-    ax4.hist(x, bins=300, align='left')
-    ax4.axvline(meanx, color='red', lw=plot_lw, ls='--')
-    ax4.axvline(stdx_line[0], color='green', lw=plot_lw, ls='--')
-    ax4.axvline(stdx_line2[0], color='green', lw=plot_lw, ls='--')
-    ax4.set_xlim(float('%.11g' % meanx) - lim_range, float('%.11g' % meanx) + lim_range)
-    ax5.hist(y, bins=300)
-    ax5.axvline(meany, color='red', lw=plot_lw, ls='--')
-    ax5.axvline(stdy_line[0], color='green', lw=plot_lw, ls='--')
-    ax5.axvline(stdy_line2[0], color='green', lw=plot_lw, ls='--')
-    ax5.set_xlim(float('%.11g' % meany) - lim_range, float('%.11g' % meany) + lim_range)
-    ax6.hist(z, bins=300)
-    ax6.axvline(meanz, color='red', lw=plot_lw, ls='--')
-    ax6.axvline(stdz_line[0], color='green', lw=plot_lw, ls='--')
-    ax6.axvline(stdz_line2[0], color='green', lw=plot_lw, ls='--')
-    ax6.set_xlim(float('%.11g' % meanz) - lim_range, float('%.11g' % meanz) + lim_range)
-
-    plt.margins(x=0)
-    plt.show()
-
 def ecef_lla(x, y, z):
     """
     """
@@ -179,6 +85,69 @@ def ecef_lla(x, y, z):
         altitude.append(alt)
 
     return lattitude, longitude, altitude
+
+def ecef_w_lines_plot(t, y, mean, mean_line, sd_line1=None, sd_line2=None, stdev=None, ax=None, **kwargs):
+    if ax is None:
+        ax.plt.gca()
+
+    ax.plot_date(t, y, marker='None', ls='-', lw=plot_lw, **kwargs)
+    ax.plot_date(t, mean_line, marker='None',color='red', lw=plot_lw, ls='--', label="average: "+'%.11g' % mean +"m")
+    ax.legend(loc='lower left', fontsize = label_size)
+    ax.set_ylim(float('%.11g' % mean) - lim_range, float('%.11g' % mean) + lim_range)
+    ax.margins(x=0)
+
+    if sd_line1 is not None:
+        ax.plot_date(t, sd_line1, marker='None',color='green', lw=plot_lw, ls='--', label="3\u03C3: +/-"+ '%.5g' % stdev +"m")
+        ax.plot_date(t, sd_line2, marker='None', color='green', lw=plot_lw, ls='--')
+
+    return(ax)
+
+def histogram_w_stdev_plot(data, mean, sd_line1, sd_line2, ax=None):
+    if ax is None:
+        ax.plt.gca()
+
+    ax.hist(data, bins=300)
+    ax.axvline(mean, color='red', lw=plot_lw, ls='--')
+    ax.axvline(sd_line1[0], color='green', lw=plot_lw, ls='--')
+    ax.axvline(sd_line2[0], color='green', lw=plot_lw, ls='--')
+    ax.set_xlim(float('%.11g' % mean) - lim_range, float('%.11g' % mean) + lim_range)
+
+    return(ax)
+
+##########DATA ANALYSIS
+def calc_ECEFstatistics(x, y, z, t):
+    """
+    """
+    fig, ((ax1, ax4), (ax2, ax5), (ax3, ax6)) = plt.subplots(3, 2, figsize=(12,8), gridspec_kw={'width_ratios': [3, 1]}) #, sharex=True
+    fig.suptitle(f"ECEF Positions from " +str(t[0]) + " to "+ str(t[-1]))
+
+    l = [x, y, z]
+    ax = [ax1, ax2, ax3]
+    ax_hist = [ax4, ax5, ax6]
+    for i in range(3):
+        mean = statistics.mean(l[i])
+        mean_line = [mean] * len(l[i])
+        stdev = statistics.pstdev(l[i])
+        stdev3 = 3*stdev
+        std_line = [mean+stdev3] *len(l[i])
+        std_line2 = [mean-stdev3] *len(l[i])
+        #plot ECEF
+        ecef_w_lines_plot(t, l[i], mean, mean_line, std_line, std_line2, stdev3, ax=ax[i])
+        #plot Histogram
+        histogram_w_stdev_plot(l[i], mean, std_line, std_line2, ax_hist[i])
+        
+    ax1.set_title("x position (m)", fontsize = title_size) 
+    ax1.set_xticklabels([])
+    ax2.set_title("y position (m)", fontsize = title_size) 
+    ax2.set_xticklabels([])
+    ax3.set_title("z position (m)", fontsize = title_size) 
+    ax3.set_xticklabels([])
+    ax3.set_xticklabels(ax3.get_xticks(), rotation = 45)
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M:%S"))
+    ax3.set(xlabel="Time")
+
+    plt.margins(x=0)
+    plt.show()
 
 def lla_plot(lat, lon):
     """
@@ -224,56 +193,27 @@ def calc_ECEFstatisticsPPK(x, y, z, t, x1, y1, z1, t1):
     x, y, z: ECEF of unprocessed file
     x1, y1, z1: PPK/RTK data (will be plotted on top of original)
     """
-    meanx = statistics.mean(x) 
-    meanx_line = [meanx] * len(x)
-    stdx = statistics.pstdev(x)
-    stdx3 = 3*stdx
-    stdx_line = [meanx+stdx*3] *len(x)
-    stdx_line2 = [meanx+stdx*-3] *len(x)
 
-    meany = statistics.mean(y) 
-    stdy = statistics.pstdev(y)
-    meany_line = [meany] * len(y)
-    stdy3 = 3*stdy
-    stdy_line = [meany+stdy*3] *len(y)
-    stdy_line2 = [meany+stdy*-3] *len(y)
-
-    meanz = statistics.mean(z) 
-    stdz = statistics.pstdev(z)
-    meanz_line = [meanz] * len(z)
-    stdz3 = 3*stdz
-    stdz_line = [meanz+stdz*3] *len(z)
-    stdz_line2 = [meanz+stdz*-3] *len(z)
-
-    #ploting
     fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(12,8)) #, sharex=True
-    fig.suptitle(f"ECEF Positions")
-    lim_range = 10
+    fig.suptitle("ECEF Positions")
 
-    ax1.plot(x, lw=plot_lw, alpha=.5, label="Unprocessed Data")
+    l = [x, y, z]
+    ax = [ax1, ax2, ax3]
+    ax_hist = [ax4, ax5, ax6]
+    for i in range(3):
+        mean = statistics.mean(l[i])
+        mean_line = [mean] * len(l[i])
+        stdev = statistics.pstdev(l[i])
+        stdev3 = 3*stdev
+        std_line = [mean+stdev3] *len(l[i])
+        std_line2 = [mean-stdev3] *len(l[i])
+        #plot ECEF
+        ecef_w_lines_plot(t, l[i], mean, mean_line, std_line=None, std_line2=None, stdev3=None, ax=ax[i], label="Unprocessed Data")
+
     ax1.plot(x1, lw=2, color='green', label="PPK Data")
-    ax1.plot(meanx_line, color='red', lw=plot_lw, ls='--', label="x average: "+'%.11g' % meanx +"m")
-    ax1.set_title("x position (m)", fontsize = title_size) #, stdev: "'%.5g' % stdx
-    ax1.legend(loc='lower left', fontsize = label_size)
-    ax1.set_ylim(float('%.11g' % meanx) - lim_range, float('%.11g' % meanx) + lim_range)
-    ax1.margins(x=0)
-    
-    ax2.plot(y, lw=plot_lw, alpha=.5, label="Unprocessed Data")
     ax2.plot(y1, lw=2, color='green', label="PPK Data")
-    ax2.plot(meany_line, color='red', lw=plot_lw, ls='--', label="y average: "+'%.11g' % meany +"m")
-    ax2.set_title("y position (m)", fontsize = title_size)
-    ax2.legend(loc='lower left', fontsize = label_size)
-    ax2.set_ylim(float('%.11g' % meany) - lim_range, float('%.11g' % meany) + lim_range)
-    ax2.margins(x=0)
-
-    ax3.plot(z, lw=plot_lw, alpha=.5, label="Unprocessed Data")
-    ax3.plot(z1, lw=2, color='green', label="PPK Data")
-    ax3.plot(meanz_line, color='red', lw=plot_lw, ls='--', label="z average: "+'%.11g' % meanz +"m")
-    ax3.set_title("z position (m)", fontsize = title_size)
-    ax3.legend(loc='lower left', fontsize = label_size)
-    ax3.set(xlabel="Time (s)")
-    ax3.set_ylim(float('%.11g' % meanz) - lim_range, float('%.11g' % meanz) + lim_range)
-    ax3.margins(x=0)
+    ax3.plot(y1, lw=2, color='green', label="PPK Data")
+    ax3.set(xlabel="Time")
 
     plt.margins(x=0)
     plt.show()
@@ -282,11 +222,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--xyz', help='Plot xyz coordinates on individual scatter plots')
     parser.add_argument('--lla', help='Plot lat vs lon')
-    parser.add_argument('--histogram', help='Plot histogram of lattitude')
-    parser.add_argument('--kinematic', type=int, help='Plots kinematic processed data')
+    parser.add_argument('--kinematic', help='Plots kinematic processed data')
     args = parser.parse_args()
 
-    print("PULLING .pos file from /tempData folder...")
+    print("PULLING .pos file[s] from /tempData folder...")
     script_dir = os.path.dirname(__file__)
     path = os.path.join(script_dir, "tempData", "*.pos") 
     file_name = glob.glob(path)
@@ -294,31 +233,25 @@ if __name__ == '__main__':
 
     # PARSE FILE(s)
     x, y, z, t = parse_file(file_name[0])
+    sample_rate = (t[1]-t[0]).total_seconds()
+    print("Sample rate: " + str(sample_rate) + "s")
 
     if args.xyz is not None:
         # ECEF PLOTS
-        x1, y1, z1, t1 = det_sample15(x, y, z, t) 
-        calc_ECEFstatistics(x1, y1, z1, t1)
+        calc_ECEFstatistics(x, y, z, t)
 
     if args.lla is not None:
         # LAT LON  DISTRIBUTION
         lat, lon, alt = ecef_lla(x1, y1, z1)
         lla_plot(lat, lon)
 
-    if args.histogram is not None:
-        # HISTOGRAM
-        plt.hist(lat, bins=300)
-        plt.show()
-
     if args.kinematic is not None: 
-        print("PPK Solution: ")
-        x1, y1, z1, t1 = parse_file(file_name[1]) #FIX THISS!!!!!!!!! 
-        x1, y1, z1, t1 = det_sample(x1, y1, z1, t1)
-        x, y, z, t = det_sample(x, y, z, t) 
-        if args.kinematic == 1:
-            calc_ECEFstatisticsPPK(x1, y1, z1, t1, x, y, z, t) # first ECEF's are unprocessed
-        else:
-            calc_ECEFstatisticsPPK(x, y, z, t, x1, y1, z1, t1)
+        print("Make sure ")
+        num = input("Which file is the receiver .pos? [0, 1, ...]? ")
+        x, y, z, t = parse_file(file_name[num]) 
+        num2 = input("Which file is the base .pos? ")
+        x1, y1, z1, t1 = parse_file(file_name[num2])
+        calc_ECEFstatisticsPPK(x, y, z, t, x1, y1, z1, t1)
 
     else:
         #default
